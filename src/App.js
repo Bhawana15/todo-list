fee3404373c4cab8a01fe21c71cd616300feacce
Bhawana15 <bhawanatiwari888@gmail.com>
2021-05-23 21:42:34 +0530

    Added todo, todos, footer files

--- src/App.js
+++ src/App.js
@@ -1,30 +1,42 @@
-import logo from './logo.svg';
 import './App.css';
+import Header from "./MyComponents/Header.js";
+import {Todos} from "./MyComponents/Todos.js";
+import {Footer} from "./MyComponents/Footer.js";
 
 function App() {
+  let todos = [
+    {
+      sno : 1,
+      title : "Go to the Market",
+      desc : "You need to go to the market to buy fruits"
+    },
+    {
+      sno : 2,
+      title : "Go to the School",
+      desc : "You need to go to the school to study"
+    },
+    {
+      sno : 3,
+      title : "Go to the College",
+      desc : "You need to go to the college to take an exam"
+    },
+    {
+      sno : 4,
+      title : "Go to the Office",
+      desc : "You need to go to office to do your job"
+    },
+    {
+      sno : 5,
+      title : "Go to the Home",
+      desc : "You need to go to the home and take rest"
+    }
+  ]
   return (
-    <nav className="navbar navbar-expand-lg navbar-light bg-light">
-      <div className="container-fluid">
-        <a className="navbar-brand" href="#">Todo-List</a>
-        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-          <span className="navbar-toggler-icon"></span>
-        </button>
-        <div className="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-            <li className="nav-item">
-              <a className="nav-link active" aria-current="page" href="#">Home</a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="#">About</a>
-            </li>
-          </ul>
-          <form className="d-flex">
-            <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
-            <button className="btn btn-outline-success" type="submit">Search</button>
-          </form>
-        </div>
-      </div>
-    </nav>
+    <>
+        <Header title="My Todo List" searchBar={true}/>
+        <Todos todos={todos}/>
+        <Footer/>
+    </>
   );
 }
 
